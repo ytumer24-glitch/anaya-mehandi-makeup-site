a1658e31dc29803c99291fca0270328216658fb7
huda-dev <ytumer24@gmail.com>
2026-05-15 06:43:03 -0700

    add js

---
+++ script.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll(".button");
+const form = document.querySelector("#contForm");
+const nameInput = document.querySelector("#name");
+const successMessage = document.querySelector("#success-msg");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert("thanks ham jald hi ap sy contact karengy");
+    });
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = nameInput.value;
+    successMessage.textContent = `Thank you, ${name}, for contacting us! We will get back to you soon.`;
+    successMessage.style.color = "green";
+    form.reset();
+});
